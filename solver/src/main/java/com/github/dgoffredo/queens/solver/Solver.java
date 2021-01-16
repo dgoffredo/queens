8f136a4f0dfb9e322316e5e1b2a57149a198ce6c
David Goffredo <dmgoffredo@gmail.com>
2021-01-16 15:52:01 -0500

    docs touchup

--- solver/src/main/java/com/github/dgoffredo/queens/solver/Solver.java
+++ solver/src/main/java/com/github/dgoffredo/queens/solver/Solver.java
@@ -1,5 +1,7 @@
 package com.github.dgoffredo.queens.solver;
 
+// Solver is the functional interface for all implementations of solutions to
+// the extended n-queens problem.
 public interface Solver {
     // Return a solution to the n-queens no-three-in-line problem, where "n" is
     // the specified sideLength. Return null if there is no solution.
